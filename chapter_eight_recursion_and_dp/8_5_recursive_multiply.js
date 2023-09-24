28246d40f87cb9acfcb413e7330ef7bfd73b9b96
Patrick Karsh <ptkarsh@gmail.com>
2023-09-24 13:06:09 -0400

    recursive multiply

--- chapter_eight_recursion_and_dp/8_5_recursive_multiply.js
+++ chapter_eight_recursion_and_dp/8_5_recursive_multiply.js
@@ -1,13 +1,28 @@
 // 8.5 Recursive multiply pg 135, solution: pg 350
 // multiply with only the sum function
 
-// create a array of arrays and sum the number
-function multiplyWithSum(a, b) {
-  return Array(a)
-    .fill(Array(b).fill(1))
-    .flat()
-    .reduce((sum, x) => sum + x);
+//  Simple Recursive Multiply
+function recursiveMultiply(a, b) {
+  if (b === 0) return 0;
+  if (b > 0) return a + recursiveMultiply(a, b - 1);
+  if (b < 0) return -recursiveMultiply(a, -b);
 }
-console.log(multiplyWithSum(9, 9));
 
+console.log(recursiveMultiply(3, 4));  // Output: 12
+console.log(recursiveMultiply(5, 0));  // Output: 0
+console.log(recursiveMultiply(-2, 3)); // Output: -6
 
+// Optimized Recursive Multiply
+function recursiveMultiplyOptimized(a, b) {
+  if (b === 0) return 0;
+  if (b < 0) return -recursiveMultiplyOptimized(a, -b);
+  
+  let half = recursiveMultiplyOptimized(a, b >> 1);
+  
+  if (b % 2 === 0) return half + half;
+  else return half + half + a;
+}
+
+console.log(recursiveMultiplyOptimized(3, 4));  // Output: 12
+console.log(recursiveMultiplyOptimized(5, 0));  // Output: 0
+console.log(recursiveMultiplyOptimized(-2, 3)); // Output: -6
--- chapter_eight_recursion_and_dp/8_6_towers_of_hanoi.js
+++ chapter_eight_recursion_and_dp/8_6_towers_of_hanoi.js
@@ -1,1 +1,66 @@
 // Towers of Hanoi: pg: 135 solution: 353
+// In the classic problem of the Towers of Hanoi, you have 3 towers and N disks of different sizes which can slide onto any tower.
+// The puzzle starts with disks sorted in ascending order of size from top to bottom (i.e., each disk sits on top of an even larger one).
+
+// Recursive solution
+function towersOfHanoi(n, source, auxiliary, target) {
+    if (n > 0) {
+        // Move n - 1 disks from source to auxiliary rod
+        towersOfHanoi(n - 1, source, target, auxiliary);
+        
+        // Move the nth disk from source to target rod
+        console.log(`Move disk ${n} from ${source} to ${target}`);
+        
+        // Move the n - 1 disks from auxiliary rod to target rod
+        towersOfHanoi(n - 1, auxiliary, source, target);
+    }
+}
+
+// Example usage:
+towersOfHanoi(3, 'A', 'B', 'C');
+
+// Iterative solution
+function towersOfHanoiIterative(n) {
+    var source = [];
+    var auxiliary = [];
+    var target = [];
+    
+    // Initialize the source rod
+    for (let i = n; i > 0; i--) {
+        source.push(i);
+    }
+    
+    // Calculate the total number of moves required
+    var totalMoves = Math.pow(2, n) - 1;
+    
+    var moves = 0;
+    var from, to;
+    while (moves < totalMoves) {
+        // Determine the from and to rods for the current move
+        if (moves % 3 === 0) {
+            from = source;
+            to = target;
+        } else if (moves % 3 === 1) {
+            from = source;
+            to = auxiliary;
+        } else {
+            from = auxiliary;
+            to = target;
+        }
+        
+        // Make a valid move between from and to rods
+        if (to.length === 0 || (from.length > 0 && from[from.length - 1] < to[to.length - 1])) {
+            to.push(from.pop());
+        } else {
+            from.push(to.pop());
+        }
+        
+        moves++;
+    }
+    
+    // Print the result
+    console.log("Target rod: ", target);
+}
+
+// Example usage:
+towersOfHanoiIterative(3);
